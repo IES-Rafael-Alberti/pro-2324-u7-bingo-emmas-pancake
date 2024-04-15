2213727c8594318623cf8b264418b7962ca020d8
dganicolas <dganicolas@rafaelalberti.es>
2024-04-15 21:37:31 +0200

    terminar carton

--- src/main/kotlin/Carton.kt
+++ src/main/kotlin/Carton.kt
@@ -6,15 +6,48 @@ class Carton {
             arrayOf(null, null, null, null, null, null, null, null, null),
             arrayOf(null, null, null, null, null, null, null, null, null)
     )
+    Triple(1, 1, 3),
+        Triple(1, 2, 2),
+        Triple(1, 3, 1),
+        Triple(2, 1, 2),
+        Triple(2, 2, 1),
+        Triple(3, 1, 1)
 */
     init {
-        println()
+        val posiciones = listaAleatoria()
     }
 
-    companion object {
-        val posiciones = listOf(0, 1, 2 ,3 ,4 ,5 ,6, 7, 8).shuffled()
+    private fun listaAleatoria(): List<Int> {
+        val numeros: List<List<Int?>> = mutableListOf(
+            mutableListOf(null, null, null, null, null, null, null, null, null),
+            mutableListOf(null, null, null, null, null, null, null, null, null),
+            mutableListOf(null, null, null, null, null, null, null, null, null)
+        )
+        val lista = listOf(0, 1, 2 ,3 ,4 ,5 ,6, 7, 8).shuffled()
+        var contador = 0
+        for (i in lista){
+            if (contador <2){
+                colocar3numeros(numeros,lista[i])
+            }else{
+                if (contador < 7){
+
+                }else{
+
+                }
+            }
+           contador++
+        }
     }
 
+    private fun colocar3numeros(numeros: List<List<Int?>>, posicion: Int) {
+        var numeros = numeros.toMutableList()
+        for (i in 0..2){
+            val lista = numeros[i].toMutableList()
+            lista[posicion]= 1
+            numeros[i] = lista
+        }
+
+    }
     private fun crearCarton() {
 
     }
