e604b58f7689dbcd29e138ff816e48d3b37f3429
dganicolas <dganicolas@rafaelalberti.es>
2024-04-17 11:31:46 +0200

    carton funciona v2

--- src/main/kotlin/Carton.kt
+++ src/main/kotlin/Carton.kt
@@ -4,14 +4,51 @@ class Carton {
     }
 
     companion object {
-        val combinaciones = listOf(
+        /**
+         * Estas son la combinaciones posibles que puede haber en el carton
+         * son permutaciones, en este caso tenemos 27 numero y tomamos 18 numeros
+         * función matemática de la permutación: (falta comprobar si esto es verdad:D)
+         * P(m,n) = m! /(m-n)!
+         * @author Nicolás De Gomar
+        */
+        private val combinaciones = listOf(
             Triple(1, 1, 3), Triple(1, 2, 2), Triple(1, 3, 1),
             Triple(2, 1, 2), Triple(2, 2, 1), Triple(3, 1, 1)
         )
+        /**
+         * esta es la combinacion elegida en este caso
+         */
         private lateinit var combinacion: Triple<Int, Int, Int>
+        /**
+         * esta variable me marca que aqui tengo que poner dos numero en la parter de arriba del carton
+         * ejemplo
+         * fila 1: 1
+         * fila 2: 1
+         * fila 3: null
+         * */
         var primerNumero:Int = 0
+        /**
+         * esta variable me marca que aqui tengo que poner dos numero en los extremos del carton
+         * ejemplo
+         * fila 1: 1
+         * fila 2: null
+         * fila 3: 1
+         * */
         var segundoNumero:Int = 0
+        /**
+         * esta variable me marca que aqui tengo que poner dos numero en la parte de abajo del carton
+         * ejemplo
+         * fila 1: null
+         * fila 2: 1
+         * fila 3: 1
+         * */
         var tercerNumero:Int = 0
+
+        /**
+         * funcion privada
+         * cogemos una combinacion aleatoria posible
+         * le asginamos esa combiancion a las 3 filas
+         * */
         private fun obtenercombinacion() {
             combinacion = combinaciones.random()
             primerNumero = combinacion.first
@@ -21,8 +58,11 @@ class Carton {
 
     }
 
+    /**
+     * función pone los numeros sobre el carton
+     * */
     private fun listaAleatoria(): List<List<Int?>> {
-        var numeros: List<List<Int?>> = mutableListOf(
+        var numeros: MutableList<MutableList<Int?>> = mutableListOf(
             mutableListOf(null, null, null, null, null, null, null, null, null),
             mutableListOf(null, null, null, null, null, null, null, null, null),
             mutableListOf(null, null, null, null, null, null, null, null, null)
@@ -54,80 +94,107 @@ class Carton {
             }
             println()
 
+
         }
-        println(numeros[0].filterNotNull().count())
-        println(numeros[1].filterNotNull().count())
-        println(numeros[2].filterNotNull().count())
+        println("----------------------")
         return numeros
     }
 
-    private fun colocar3numeros(numeros: List<List<Int?>>, posicion: Int): MutableList<List<Int?>> {
-        val numero = numeros.toMutableList()
-        for (i in 0..2) {
-            val lista = numeros[i].toMutableList()
-            lista[posicion] = 1
-            numero[i] = lista
+    private fun obtenerNumeros(numero :Int,cantidad:Int): List<Int> {
+        val numeros = mutableListOf<Int>()
+        var contador = 0
+        if ( numero == 0){
+            val lista = mutableListOf<Int>()
+            for (i in 1..9){
+                lista.add(i)
+            }
+            while (contador != cantidad){
+                val number = lista.random().toString().toInt()
+                if (number !in numeros){
+                    numeros.add(number)
+                    contador++
+                }
+            }
         }
-        return numero
+        if ( numero == 8){
+            val lista = mutableListOf<Int>()
+            for (i in 80..90){
+                lista.add(i)
+            }
+            while (contador != cantidad){
+                val number = lista.random().toString().toInt()
+                if (number !in numeros){
+                    numeros.add(number)
+                    contador++
+                }
+            }
+        }
+        else{
+            val lista = mutableListOf<Int>()
+            for (i in numero*10..numero*10+9){
+                lista.add(i)
+            }
+            while (contador != cantidad){
+                val number = lista.random().toString().toInt()
+                if (number !in numeros){
+                    numeros.add(number)
+                    contador++
+                }
+            }
+        }
+        return numeros
     }
 
-    private fun colocar2numeros(numeros: List<List<Int?>>, posicion: Int): MutableList<List<Int?>> {
-        val numero = numeros.toMutableList()
+    private fun colocar3numeros(numeros: MutableList<MutableList<Int?>>, posicion: Int): MutableList<MutableList<Int?>> {
+        val numero= obtenerNumeros(posicion,3).sorted()
+        for (i in 0..2) {
+            numeros[i][posicion] = numero[i]
+        }
+        return numeros
+    }
+
+    private fun colocar2numeros(numeros: MutableList<MutableList<Int?>>, posicion: Int): MutableList<MutableList<Int?>> {
+        val numero= obtenerNumeros(posicion,2).sorted()
         if (primerNumero != 0) {
             primerNumero -= 1
             for (i in 0..1) {
-                val lista = numero[i].toMutableList()
-                lista[posicion] = 1
-                numero[i] = lista
+                numeros[i][posicion] = numero[i]
             }
         } else {
             if (segundoNumero != 0) {
                 segundoNumero -= 1
-                var lista = numero[0].toMutableList()
-                lista[posicion] = 1
-                numero[0] = lista
-                lista = numero[2].toMutableList()
-                lista[posicion] = 1
-                numero[2] = lista
+                numeros[0][posicion] = numero[0]
+                numeros[2][posicion] = numero[1]
             } else {
                 if (tercerNumero != 0) {
                     tercerNumero -= 1
                     for (j in 1..2) {
-                        val lista = numero[j].toMutableList()
-                        lista[posicion] = 1
-                        numero[j] = lista
+                        numeros[j][posicion] = numero[j-1]
                     }
                 }
             }
         }
-        return numero
+        return numeros
     }
 
-    private fun colocar1numeros(numeros: List<List<Int?>>, posicion: Int): MutableList<List<Int?>> {
+    private fun colocar1numeros(numeros: MutableList<MutableList<Int?>>, posicion: Int): MutableList<MutableList<Int?>> {
 
-        val numero = numeros.toMutableList()
-        val fila0 = numero[0].filterNotNull().count()
-        val fila1 = numero[1].filterNotNull().count()
-        val fila2 = numero[2].filterNotNull().count()
-        val lista: MutableList<Int?>
+        val numero = obtenerNumeros(posicion,1)
+        val fila0 = numeros[0].filterNotNull().count()
+        val fila1 = numeros[1].filterNotNull().count()
+        val fila2 = numeros[2].filterNotNull().count()
         if (fila0 != 6 && fila0 < 6){
-            lista = numeros[0].toMutableList()
-            lista[posicion] = 1
-            numero[0] = lista
+            numeros[0][posicion] = numero[0]
         }else{
             if (fila1 != 6 && fila1 < 6){
-                lista = numeros[1].toMutableList()
-                lista[posicion] = 1
-                numero[1] = lista
+                numeros[1][posicion] = numero[0]
             }else{
                 if (fila2 != 6 && fila2 < 6){
-                    lista = numeros[2].toMutableList()
-                    lista[posicion] = 1
-                    numero[2] = lista
+                    numeros[2][posicion] = numero[0]
                 }
             }
         }
-        return numero
+        return numeros
     }
 
     private fun crearCarton() {
